74889d03d1e43444ab834f228509547f0526ed98
Rick van der Wolk <rickvanderwolk@gmail.com>
2025-02-22 00:00:53 +0100

    Fix e2e tests - replace note ids with uuids

--- src/cypress.config.js
+++ src/cypress.config.js
@@ -8,10 +8,22 @@ export default defineConfig({
               user1: {
                   email: "user1@example.com",
                   password: "password1",
+                  notes: {
+                      first_note: {
+                          uuid: 'df6da9cb-9160-4a40-ae21-f041a79bca2c',
+                          content: 'Note 1 - user 1',
+                      }
+                  }
               },
               user2: {
                   email: "user2@example.com",
                   password: "password2",
+                  notes: {
+                      first_note: {
+                          uuid: 'b161a8a7-82c3-4ef3-aab2-27aad88b0024',
+                          content: 'Note 1 - user 2',
+                      }
+                  }
               },
           },
       },
--- src/cypress/e2e/authorization.cy.js
+++ src/cypress/e2e/authorization.cy.js
@@ -4,14 +4,14 @@ describe('Authorization Tests', () => {
         const user = Cypress.env("users").user2;
         cy.login(user.email, user.password);
         cy.visit(`${Cypress.config('baseUrl')}`);
-        cy.contains('Note 1 - user 2').should('be.visible');
+        cy.contains(user.notes.first_note.content).should('be.visible');
     });
 
     it('User 2 should be able to access User 2’s notes via direct URL', () => {
         const user = Cypress.env("users").user2;
         cy.login(user.email, user.password);
         cy.request({
-            url: `${Cypress.config('baseUrl')}/notes/2`,
+            url: `${Cypress.config('baseUrl')}/notes/${user.notes.first_note.uuid}`,
             failOnStatusCode: false,
         }).then((response) => {
             expect(response.status).to.be.oneOf([200]);
@@ -20,16 +20,18 @@ describe('Authorization Tests', () => {
 
     it('User 2 should NOT be able to see User 1’s notes', () => {
         const user = Cypress.env("users").user2;
+        const differentUser = Cypress.env("users").user1;
         cy.login(user.email, user.password);
         cy.visit(`${Cypress.config('baseUrl')}`);
-        cy.contains('Note 1 - user 1').should('not.exist');
+        cy.contains(differentUser.notes.first_note.content).should('not.exist');
     });
 
     it('User 2 should NOT be able to access User 1’s notes via direct URL', () => {
         const user = Cypress.env("users").user2;
+        const differentUser = Cypress.env("users").user1;
         cy.login(user.email, user.password);
         cy.request({
-            url: `${Cypress.config('baseUrl')}/notes/1`,
+            url: `${Cypress.config('baseUrl')}/notes/${differentUser.notes.first_note.uuid}`,
             failOnStatusCode: false,
         }).then((response) => {
             expect(response.status).to.be.oneOf([403, 404]);
