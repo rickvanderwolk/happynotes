094b96c90d3f326f97ecc0e5cb2ebb178138b38e
Rick van der Wolk <rickvanderwolk@gmail.com>
2025-02-21 22:51:50 +0100

    Add Cypress command for login + add base URL to Cypress config

--- src/cypress.config.js
+++ src/cypress.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+      baseUrl: "http://127.0.0.1:8001",
+      setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
