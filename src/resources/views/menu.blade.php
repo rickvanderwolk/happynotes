3a9637b7df5f1786be76d763a033a056b2ff75ad
Rick van der Wolk <rickvanderwolk@gmail.com>
2025-02-22 10:56:05 +0100

    Display app version in menu

--- src/resources/views/menu.blade.php
+++ src/resources/views/menu.blade.php
@@ -1,16 +1,22 @@
 <x-app-layout>
-    <ul id="menu" class="list-group">
-        <a href="{{ route('profile.show') }}" data-turbolinks="false">
+    <div>
+        <ul id="menu" class="list-group">
+            <a href="{{ route('profile.show') }}" data-turbolinks="false">
+                <li class="list-group-item text-center">
+                    Account
+                    <i class="fa fa-up-right-from-square"></i>
+                </li>
+            </a>
             <li class="list-group-item text-center">
-                Account
-                <i class="fa fa-up-right-from-square"></i>
+                <form method="POST" action="{{ route('logout') }}" style="display: inline;">
+                    @csrf
+                    <button id="logout-button" type="submit" class="btn btn-link p-0 m-0 align-baseline" style="text-decoration: none; color: inherit;">Logout</button>
+                </form>
             </li>
-        </a>
-        <li class="list-group-item text-center">
-            <form method="POST" action="{{ route('logout') }}" style="display: inline;">
-                @csrf
-                <button id="logout-button" type="submit" class="btn btn-link p-0 m-0 align-baseline" style="text-decoration: none; color: inherit;">Logout</button>
-            </form>
-        </li>
-    </ul>
+        </ul>
+
+        <div class="flex items-center justify-center text-center mt-4">
+            {{ config('app.version') }}
+        </div>
+    </div>
 </x-app-layout>
