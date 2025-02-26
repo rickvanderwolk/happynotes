5be12bd238984dc87a2593b346298cfc1575d73a
Rick van der Wolk <rickvanderwolk@gmail.com>
2025-02-26 20:14:00 +0100

    Fix iOS status bar and address bar color in darkmode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)">
+    <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)">
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="turbolinks-cache-control" content="no-preview">
 
