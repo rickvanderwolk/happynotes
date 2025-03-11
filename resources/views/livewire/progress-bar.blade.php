7580acd7d4762821d22de74c750ea8229758df08
Rick van der Wolk <rickvanderwolk@gmail.com>
2025-03-11 21:23:40 +0100

    Add aria-label to progress bar for improved accessibility

--- resources/views/livewire/progress-bar.blade.php
+++ resources/views/livewire/progress-bar.blade.php
@@ -6,7 +6,8 @@
                  style="width: {{ $progress }}%"
                  aria-valuenow="{{ $progress }}"
                  aria-valuemin="0"
-                 aria-valuemax="100">
+                 aria-valuemax="100"
+                 aria-label="Progress: {{ $progress }}%">
             </div>
         </div>
     @endif
